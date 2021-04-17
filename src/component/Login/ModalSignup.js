ed496eec4b4a2de4d9fb3167cdc95f09ed0e1ca0
Nguyễn Ngọc Duy <happyduy69@gmail.com>
2021-04-18 02:37:23 +0700

    Add alert when creating account

--- src/component/Login/ModalSignup.js
+++ src/component/Login/ModalSignup.js
@@ -62,6 +62,28 @@ export default class ModalSignup extends Component {
         .then((res) => {
           console.log(res);
           console.log(res.data);
+          if (res.status < 300 && res.status > 199) {
+            alert("Account created! Please login.");
+          }
+        })
+        .catch(function (error) {
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+            alert(error.response.data.message);
+          } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            console.log(error.request);
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+          }
+          console.log(error.config);
         });
     } else {
       alert(Object.values(this.state.errors));
